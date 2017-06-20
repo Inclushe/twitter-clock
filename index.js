dfb6116c2c1f3f9383c20719163d9ca96e2a0579
Ethan <inclushe@gmail.com>
2017-06-20 07:37:26 +0100

    Functional

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
 var fs = require('fs')
 var gm = require('gm')
-var name = 'temp/' + Math.round(Math.random() * 100000) + '.png'
 
-gm(1000, 1000, '#41232100')
-  .draw('image Over 0,0 1000,1000 hour_hand.jpg')
-  //.geometry('+10+100')
-  .write(name, err => {
-    if (err) {
-      console.error(err.message)
-      return
-    }
-    require('child_process').execSync(`"C:/Program Files (x86)/Microsoft VS Code/Code.exe" ${name}`)
-  })
+var currentTime = new Date()
+var currentHour = currentTime.getHours() % 12
+var currentHourDeg = (360 / 12 * currentHour)
+var currentMinutes = currentTime.getMinutes()
+var currentMinutesDeg = (360 / 60 * currentMinutes)
+
+var name = `temp/${currentHour}${currentMinutes}.png`
+
+function handleError (err) {
+  if (err) {
+    console.error(err.message)
+    return
+  }
+}
+
+function createClockAvatar () {
+  drawHourHand()
+
+  function drawHourHand () {
+    gm(1000, 1000, 'transparent')
+      .draw('image Over 0,0 1000,1000 hour_hand.png')
+      .rotate('transparent', currentHourDeg + (currentMinutesDeg / 12))
+      .gravity('Center')
+      .crop(1000, 1000)
+      .write('temp/hour.png', (err) => {
+        handleError(err)
+        drawMinuteHand()
+    })
+  }
+
+  function drawMinuteHand () {
+    gm(1000, 1000, 'transparent')
+      .draw('image Over 0,0 1000,1000 minute_hand.png')
+      .rotate('transparent', currentMinutesDeg)
+      .gravity('Center')
+      .crop(1000, 1000)
+      .write('temp/minute.png', (err) => {
+        handleError(err)
+        drawClock()
+      })
+  }
+
+  function drawClock () {
+    gm(1000, 1000, '#333333')
+      .draw('image Over 0,0 1000,1000 temp/hour.png')
+      .draw('image Over 0,0 1000,1000 temp/minute.png')
+      .write(name, err => {
+        handleError(err)
+        console.log('clock')
+        fs.unlink('temp/hour.png', handleError)
+        fs.unlink('temp/minute.png', handleError)
+        require('child_process').execSync(`"C:/Program Files (x86)/Microsoft VS Code/Code.exe" ${name}`)
+      })
+  }
+}
+
+createClockAvatar()
